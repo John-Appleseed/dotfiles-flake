3ee41e5f8070e5d6d27e81639e54a1aedf0939a2
John-Appleseed <JohnAppleseed@wovac.com>
2023-04-15 18:18:37 -0500

    Update default.nix

--- hosts/exampleHost/default.nix
+++ hosts/exampleHost/default.nix
@@ -11,26 +11,13 @@
       devNodes = "/dev/disk/by-id/";
       bootDevices = [ "bootDevices_placeholder" ];
       immutable = false;
-      availableKernelModules = [
-        # for booting virtual machine
-        # with virtio disk controller
-        "virtio_pci"
-        "virtio_blk"
-        # for sata drive
-        "ahci"
-        # for nvme drive
-        "nvme"
-        # for external usb drive
-        "uas"
-        # for mmc drive
-        "rtsx_usb_sdmmc"
-      ];
+      availableKernelModules = [ "availableKernelModules_placeholder" ];
       removableEfi = true;
       kernelParams = [ ];
       sshUnlock = {
         # read sshUnlock.txt file.
-        enable = false;
-        authorizedKeys = [ ];
+        enable = true;
+        authorizedKeys = [ "authorizedKeys_placeholder" ];
       };
     };
     networking = {
